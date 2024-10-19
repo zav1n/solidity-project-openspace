7ab7a79e4cf09604142a314b6245c56cc51a1e22
zaven <2avvv1n@gmail.com>
2024-10-19 13:27:16 +0800

    fix: 修复DeployMyToken脚本并添加广播文件

--- foundry_project/script/DeployMyToken.s.sol
+++ foundry_project/script/DeployMyToken.s.sol
@@ -4,24 +4,21 @@ pragma solidity 0.8.25;
 import "forge-std/Script.sol";
 import "../src/MyToken.sol";
 
-// forge script script/DeployMyToken.s.sol --rpc-url ETH_RPC_URL --broadcast --verify -vvvv --account 8888
-
-
 
 /**
+    forge 跑script脚本 目录 广播 校验 校验者帐户 帐户名称(之前提前设置好的,可以forge wallet list 查看)
+    forge script script/DeployMyToken.s.sol --broadcast --verify --account 8888
 
-forge verify-contract --chain sepolia --constructor-args 
-"$(cast abi-encode 'constructor(string memory name_, string memory symbol_)' MyToken MTK)" 
-0x1e5e67907f0c08871701e54099c1e68635f62b43 MyToken Start verifying contract `0x1e5e67907f0c08871701e54099c1e68635f62b43` 
-deployed on sepolia   
 
  */
+
 
 contract DeployMyToken is Script {
     function run() external {
         string memory name = "MyToken";
         string memory symbol = "MTK";
-
+        // 如果createSelectFork这个没有设置,则命令需要配置 --rpc-url
+        vm.createSelectFork("sepolia");
         vm.startBroadcast();
         new MyToken(name, symbol);
         vm.stopBroadcast();
