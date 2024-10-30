9d966df6629217b871aecd9a395c89c077c0dbbe
zaven <2avvv1n@gmail.com>
2024-10-30 22:24:33 +0800

    feat: the graph - NFTMarket

--- thegraph/nftmarketv3/src/nft-market-v-3.ts
+++ thegraph/nftmarketv3/src/nft-market-v-3.ts
@@ -1,56 +1,40 @@
 import {
   Cancel as CancelEvent,
-  EIP712DomainChanged as EIP712DomainChangedEvent,
   List as ListEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  SetFeeTo as SetFeeToEvent,
-  SetWhiteListSigner as SetWhiteListSignerEvent,
   Sold as SoldEvent
 } from "../generated/NFTMarketV3/NFTMarketV3"
 import {
-  Cancel,
-  EIP712DomainChanged,
-  List,
-  OwnershipTransferred,
-  SetFeeTo,
-  SetWhiteListSigner,
-  Sold
-} from "../generated/schema"
+  // Cancel,
+  // List,
+  // Sold,
+  OrderBook,
+  FilledOrder
+} from "../generated/schema";
+
+import { Bytes } from "@graphprotocol/graph-ts";
 
 export function handleCancel(event: CancelEvent): void {
-  let entity = new Cancel(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.orderId = event.params.orderId
+  // 更新 OrderBook 的 cancelTxHash
+  let orderId = event.params.orderId; // 查询对应的 OrderBook 实体
+  let entity = OrderBook.load(orderId);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (entity) {
+    // 更新 cancelTxHash
+    entity.cancelTxHash = event.transaction.hash;
 
-  entity.save()
+    // 保存更新后的实体
+    entity.save();
+  }
 }
 
-export function handleEIP712DomainChanged(
-  event: EIP712DomainChangedEvent
-): void {
-  let entity = new EIP712DomainChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
 
 export function handleList(event: ListEvent): void {
-  let entity = new List(
+  let entity = new OrderBook(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  );
   entity.nft = event.params.nft
   entity.tokenId = event.params.tokenId
-  entity.orderId = event.params.orderId
+  // entity.orderId = event.params.orderId
   entity.seller = event.params.seller
   entity.payToken = event.params.payToken
   entity.price = event.params.price
@@ -60,62 +44,36 @@ export function handleList(event: ListEvent): void {
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
-  entity.save()
-}
+  entity.cancelTxHash = Bytes.fromHexString("0x00");
+  entity.filledTxHash = Bytes.fromHexString("0x00");
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetFeeTo(event: SetFeeToEvent): void {
-  let entity = new SetFeeTo(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.to = event.params.to
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetWhiteListSigner(event: SetWhiteListSignerEvent): void {
-  let entity = new SetWhiteListSigner(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.signer = event.params.signer
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
   entity.save()
 }
 
 export function handleSold(event: SoldEvent): void {
-  let entity = new Sold(
+  let entity = new FilledOrder(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.orderId = event.params.orderId
-  entity.buyer = event.params.buyer
-  entity.fee = event.params.fee
+  );
+  // entity.orderId = event.params.orderId;
+  entity.buyer = event.params.buyer;
+  entity.fee = event.params.fee;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  // 更新 OrderBook 的 filledTxHash
+  let orderId = event.params.orderId;
+  let orderEntity = OrderBook.load(orderId);
+
+  if (orderEntity) {
+    // 更新 filledTxHash
+    orderEntity.filledTxHash = event.transaction.hash;
+
+    // 保存更新后的 OrderBook 实体
+    orderEntity.save();
+  }
 }
