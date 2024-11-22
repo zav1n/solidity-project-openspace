23681e00bd34bd17bc30722610bd077b0ec33f32
zaven <2avvv1n@gmail.com>
2024-11-22 22:08:16 +0800

    feat: createWallet

--- react_frontend/utils/createAccount.ts
+++ react_frontend/utils/createAccount.ts
@@ -17,4 +17,21 @@ async function generateNewAddress(isloop = false) {
   }
 }
 
+// A B C D E F
+async function creatWallet() {
+  setInterval(() => {
+    let wallet; // 钱包
+    const regex = /^0xFF.FFFF$/; // 表达式
+    let isValid = false;
+    while (!isValid) {
+      wallet = ethers.Wallet.createRandom(); // 随机生成钱包，安全
+      isValid = regex.test(wallet.address); // 检验正则表达式
+    }
+    // 打印靓号地址与私钥
+    console.log(`靓号地址：${wallet.address}`);
+    console.log(`靓号私钥：${wallet.privateKey}`);
+  }, 0);
+}
+
+creatWallet();
 generateNewAddress(true);
